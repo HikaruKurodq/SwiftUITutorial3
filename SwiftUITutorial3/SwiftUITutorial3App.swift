f9a812ce6e34fed3875ef7b3fe4f792bf9ee8d2f
HikaruKuroda <h1kqaa@gmail.com>
2021-06-18 14:22:29 +0900

    First

---
+++ SwiftUITutorial3/SwiftUITutorial3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorial3App.swift
+//  SwiftUITutorial3
+//
+//  Created by HikaruKuroda on 2021/06/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorial3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
